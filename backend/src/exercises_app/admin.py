718f7134693916805211316c9b11399db0a5d4c0
hls-sml <hilliassmirali@gmail.com>
2025-06-16 20:40:02 +0200

    Add the ExerciseCategory and Exercise classes in the admin panel

--- backend/src/exercises_app/admin.py
+++ backend/src/exercises_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from exercises_app.models import CategoryExercise, Exercise
+
 # Register your models here.
+admin.site.register(CategoryExercise)
+admin.site.register(Exercise)
