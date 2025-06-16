45fde1a473ffbf4b57ee0cbec62e83eba8402416
hls-sml <hilliassmirali@gmail.com>
2025-06-16 21:41:04 +0200

    Add sluug both CategoryExercise and Exercise

--- backend/src/exercises_app/models.py
+++ backend/src/exercises_app/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class CategoryExercise(models.Model):
     name = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         verbose_name = "Category Exercise"
         verbose_name_plural = "Category Exercises"
         ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name or "Unnamed Category"
@@ -25,6 +32,7 @@ class Exercise(models.Model):
     ]
 
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField(blank=True)
     difficulty = models.CharField(
         max_length=10,
@@ -44,6 +52,11 @@ class Exercise(models.Model):
         verbose_name = "Exercise"
         verbose_name_plural = "Exercises"
         ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
