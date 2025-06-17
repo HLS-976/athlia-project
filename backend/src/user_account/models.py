6c002e2ed10a0c3cc18f8105bf129a0237116f1c
hls-sml <hilliassmirali@gmail.com>
2025-06-17 15:20:57 +0200

    Add username field

--- backend/src/user_account/models.py
+++ backend/src/user_account/models.py
@@ -5,22 +5,24 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, first_name, last_name, **extra_fields):
+    def create_user(self, email, password, user_name, first_name, last_name, **extra_fields):
         if not email:
             raise ValueError("The email is required")
+        if not user_name:
+            raise ValueError("The user name is required")
         if not first_name:
             raise ValueError("The first name is required.")
         if not last_name:
             raise ValueError("The last name is required.")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(email=email, user_name=user_name, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
 
-    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+    def create_superuser(self, email, password, user_name, first_name, last_name, **extra_fields):
         if not first_name:
             raise ValueError("The superuser must have first_name.")
         if not last_name:
@@ -29,7 +31,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        super_user = self.create_user(email=email, password=password, first_name=first_name, last_name=last_name, **extra_fields)
+        super_user = self.create_user(email=email, password=password, user_name=user_name, first_name=first_name, last_name=last_name, **extra_fields)
 
         super_user.save()
         return super_user
@@ -38,13 +40,14 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
+    user_name = models.CharField(max_length=150, )
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name", "user_name"]
 
     objects = CustomUserManager()
 
