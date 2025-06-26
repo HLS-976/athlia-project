932b326e0781cee4b0059d4d8ca5042103d4cf9b
hls-sml <hilliassmirali@gmail.com>
2025-06-26 13:22:46 +0200

    Add appp

--- backend/src/athlia_project/urls.py
+++ backend/src/athlia_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('user_account.urls')),
-    path('api/', include('exercises_app.urls'))
+    path('api/', include('user_account.urls')),
+    path('api/', include('exercises_app.urls')),
+    path('api/', include('sport_profile.urls'))
 ]
