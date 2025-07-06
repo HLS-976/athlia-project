edbaac6fcb87a51d68deb7972dcd0ac0fb0d95a9
knarta <charquen10@gmail.com>
2025-07-06 20:08:48 +0200

    Remove Header component from NotFoundPage to streamline the codebase and enhance maintainability.

--- frontend/src/Pages/NotFound/NotFoundPage.jsx
+++ frontend/src/Pages/NotFound/NotFoundPage.jsx
@@ -1,12 +1,8 @@
 import "./NotFound.css";
-import Header from "./Header";
 
 function NotFound() {
   return (
     <main>
-      <header>
-        <Header />
-      </header>
       <div id="error404">
         <h1 id="tittle">Error</h1>
         <p id="message">This page was not found</p>
