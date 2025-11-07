34a9a44168f8a36ab1f5c95b56d648a275f99ced
hls <hilliassmirali@gmail.com>
2025-11-07 20:44:18 +0100

    Update user_account

--- backend/src/user_account/views.py
+++ backend/src/user_account/views.py
@@ -34,6 +34,30 @@ class UserDetailView(APIView):
         serializer = UserDetailSerializer(request.user)
         return Response(serializer.data)
 
+    def patch(self, request, format=None):
+        serializer = UserUpdateSerializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            # On renvoie la vue "détail" standard après update
+            return Response(UserDetailSerializer(request.user).data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, format=None):
+        serializer = UserUpdateSerializer(
+            request.user,
+            data=request.data,
+            partial=False
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(UserDetailSerializer(request.user).data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
